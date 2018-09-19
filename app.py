08c8721100cc93aa52779e2c39d61961fd25b095
Daniyaal Khan <khandaniyaal2000@gmail.com>
2018-09-19 21:19:09 +0530

    Listed all endpoints

--- app.py
+++ app.py
@@ -1,6 +1,73 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/getwall')
+def hello_world():
+    return 'Hello, World!'
+@app.route('/getlike')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/postlike')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getleaderboard')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/postpoint')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getpoint')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getschedule')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/posteventlike')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/geteventlike')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getclubs')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getcoreteam')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getsponsor')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/gettambolanumber')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/posttambolaresult')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getquiz')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/postprofile')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getprofile')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/postwall')
 def hello_world():
     return 'Hello, World!'
